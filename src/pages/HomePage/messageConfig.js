527aec08be0527d6a2a6f3791a7b277c058d97cf
Abdullah-Elmorshedy <aelmorshedy@beyond-creation.net>
2026-03-15 01:58:06 +0200

    change text to another name

--- src/pages/HomePage/messageConfig.js
+++ src/pages/HomePage/messageConfig.js
@@ -1,6 +1,6 @@
 // Edit the message shown in the quote section
 export const MESSAGE_TITLE = "Message for you";
-export const MESSAGE_TEXT = `Happy birthday to you, Farah, my love, the light of my eyes, and the most beautiful girl in the whole world. ❤️🎉
+export const MESSAGE_TEXT = `Happy birthday to you, Raneem, my love, the light of my eyes, and the most beautiful girl in the whole world. ❤️🎉
 Today is a very special day because it’s the day the most amazing person in my life was born. Every day with you is a blessing, and I feel so lucky and grateful to have you in my life.
 
 You are not just someone I love… you are my soul, my heart, my happiness, and my peace. You are the person who makes my days brighter, my smile bigger, and my life more meaningful. I honestly can’t imagine my life without you in it.
@@ -9,4 +9,4 @@ I wish for you a year full of happiness, success, laughter, and beautiful moment
 
 Thank you for being the incredible person that you are, for your love, your support, and for making my life so much better just by being in it. I will always appreciate you, care for you, and stand by your side.
 
-Happy birthday again, my beautiful Farah. You will always be my love, my heart, and my everything. I hope this year brings you endless joy and love, just like the joy you bring to my life every single day. 💫❤️`;
+Happy birthday again, my beautiful Raneem. You will always be my love, my heart, and my everything. I hope this year brings you endless joy and love, just like the joy you bring to my life every single day. 💫❤️`;
